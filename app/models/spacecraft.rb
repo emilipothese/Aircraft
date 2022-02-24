ac23706d1a59968bf1c67c7131fe52a12f579eb1
Camille Hug <hug.camille@hotmail.fr>
2022-02-24 20:42:56 +0100

    Devise correction: ok

--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -1,5 +1,6 @@
 class Spacecraft < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
 
   validates :name, :capacity, :category, :location, :description, :user, presence: true
   validates :name, uniqueness: true
